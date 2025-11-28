6a737bd06ee1f0027708adee0350179d6ff573ba
Hanwen Zhang <hwenzhang@Admins-Mac-Studio.local>
2025-11-28 19:28:37 +0000

    In progress.

---
+++ src/nevis_label_blister.m
@@ -0,0 +1,88 @@
+function [gg] = nevis_label_blister(gg,d_inds,oo)
+% label makes the boundary labels and list of boundary indices
+% 
+% inputs
+%   gg grid structure [see below for contents]
+%   d_inds indices of nodes at which pressure prescribed
+%   oo [optional] option structure with method to use [see below for options]
+% outputs:
+%   gg grid structure [added/modified labels]
+
+if nargin<4, oo = struct; end
+
+%% grid point labels [ labelled first along x axis then along y axis, so location i,j becomes i+(j-1)*I ]
+ns = (1:gg.nIJ)';
+es = (1:gg.eIJ)';
+fs = (1:gg.fIJ)';
+cs = (1:gg.cIJ)';
+
+%% labels of boundary nodes, edges, corners
+nout = gg.nout;
+n1 = gg.n1;
+e0 = gg.e0;
+e1 = gg.e1;
+e2 = gg.e2;
+f0 = gg.f0;
+f1 = gg.f1;
+f2 = gg.f2;
+c0 = gg.c0;
+c1 = gg.c1;
+c2 = gg.c2;
+c3 = gg.c3;
+
+%% prescribe pressure at nodes specified by d_inds [ eliminate connected boundary edges ]
+nbdy = intersect(n1,d_inds);    % only include d_inds that are on the boundary
+
+ed1 = find(gg.emean*sparse(nbdy_blister,1,1,gg.nIJ,1)==0.5); ed1 = intersect(ed1,e1); % connected boundary edges
+ed2 = find(gg.emean*sparse(nbdy_blister,1,1,gg.nIJ,1)==1); ed2 = intersect(ed2,e2); % connected along-boundary edges
+ed = union(ed1,ed2);
+fd1 = find(gg.fmean*sparse(nbdy_blister,1,1,gg.nIJ,1)==0.5); fd1 = intersect(fd1,f1);
+fd2 = find(gg.fmean*sparse(nbdy_blister,1,1,gg.nIJ,1)==1); fd2 = intersect(fd2,f2); % connected along-boundary edges
+fd = union(fd1,fd2);
+
+ebdy = setdiff(e1,ed); eout = union(e0,ed); 
+fbdy = setdiff(f1,fd); fout = union(f0,fd);
+cbdy = []; cout = unique([c0;c1;c2;c3]);
+
+% nbdy = setdiff(nbdy,nout);   
+% ebdy = setdiff(ebdy,eout);   
+% fbdy = setdiff(fbdy,fout);   
+% cbdy = setdiff(cbdy,cout); 
+
+%% define active and interior labels
+% 20 Nov 2025: The template below is for hydrology models
+ns = setdiff(ns,nout);  %  
+es = setdiff(es,eout);   
+fs = setdiff(fs,fout);   
+cs = setdiff(cs,cout);  
+
+nin = setdiff(ns,nbdy);   
+ein = setdiff(es,ebdy);  
+fin = setdiff(fs,fbdy);  
+cin = setdiff(cs,cbdy);  
+
+% internal+bound elements
+gg.ns_blister = ns;
+gg.es_blister = es;
+gg.fs_blister = fs;
+gg.cs_blister = cs;
+
+% bound elements
+gg.nbdy_blister = nbdy;
+gg.ebdy_blister = ebdy;
+gg.cbdy_blister = cbdy;
+gg.fbdy_blister = fbdy;
+
+% internal elements
+gg.nin_blister = nin;
+gg.ein_blister = ein;
+gg.fin_blister = fin;
+gg.cin_blister = cin;
+
+% external elements
+gg.nout_blister = nout;
+gg.eout_blister = eout;
+gg.fout_blister = fout;
+gg.cout_blister = cout;
+
+end
